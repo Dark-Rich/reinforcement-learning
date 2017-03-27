b38a38f537880bb0da8053fe3e586ea05363a457
Dark-Rich <tlzhatao@gmail.com>
2017-03-27 22:18:24 +0800

    add mdp.py mdp_value.py mdp.png

---
+++ mdp/mdp.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 20:03:56 2017
+
+@author: Administrator
+"""
+
+import numpy as np
+import random
+
+class Mdp:
+    def __init__(self):
+        self.states = [1,2,3,4,5,6,7,8]
+        self.teminal_states = dict()
+        self.teminal_states[6] = 1
+        self.teminal_states[7] = 1
+        self.teminal_states[8] = 1
+        
+        self.actions = ['n','e','s','w']
+        
+        self.rewards = dict()
+        self.rewards['1_s'] = -1.0
+        self.rewards['3_s'] = 1.0
+        self.rewards['5_s'] = -1.0
+        
+        self.t = dict()
+        self.t['1_s'] = 6
+        self.t['2_e'] = 2
+        self.t['2_w'] = 1
+        self.t['2_e'] = 3
+        self.t['3_s'] = 7
+        self.t['3_w'] = 2
+        self.t['3_e'] = 4
+        self.t['4_w'] = 3
+        self.t['4_e'] = 5
+        self.t['5_s'] = 8
+        self.t['5_w'] = 4
+        
+        self.gamma = 0.8
+    
+    def transform(self,state,action):
+        if state in self.teminal_states:
+            return True,state,0
+        
+        key = '%d_%s' % (state,action)
+        if key in self.t:
+            next_state = self.t[key]
+        else:
+            next_state = state
+        
+        is_teminal = False
+        if next_state in self.teminal_states:
+            is_teminal = True
+        
+        if key not in self.rewards:
+            r = 0.0
+        else:
+            r = self.rewards[key]
+        
+        return is_teminal,next_state,r
---
+++ mdp/mdp_value.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 21:09:18 2017
+
+@author: Administrator
+"""
+
+from mdp import *
+
+random.seed(0)
+
+def random_pi():
+    actions = ['n','w','e','s']
+    r = int(random.random() * 4)
+    return actions[r]
+
+def compute_random_pi_state_value():
+    value = [0.0 for r in xrange(9)]
+    #大数模拟求均值
+    num = 1000000
+    
+    for k in xrange(1,num):
+        for i in xrange(1,6):
+            mdp = Mdp()
+            s = i
+            is_terminal = False
+            gamma = 1.0
+            v = 0.0
+            while False == is_terminal:
+                a = random_pi()
+                is_terminal,s,r = mdp.transform(s,a)
+                v += gamma * r
+                gamma *= 0.5
+            
+            value[i] = (value[i] * (k-1) + v) / k
+        if k % 10000 == 0:
+            print value[1:9]
+    print value[1:9]
+
+def main():
+    #计算随机策略下的状态值函数
+    compute_random_pi_state_value()
+
+if __name__ == '__main__':
+    main()
